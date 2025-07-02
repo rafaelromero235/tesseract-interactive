81433462716c9088d7b9d56f97466b3dc4edc2cc
rafael romero <rafaelromero235@gmail.com>
2025-07-02 21:12:18 +0200

    Primer commit - proyecto Mystical Tactics

--- team.js
+++ team.js
@@ -1,26 +1,3 @@
-const carousel = document.querySelector(".carousel");
-const items = document.querySelectorAll(".carousel-item");
-const prevBtn = document.querySelector(".carousel-btn.prev");
-const nextBtn = document.querySelector(".carousel-btn.next");
-
-let currentIndex = 0;
-
-function updateCarousel() {
-  const offset = -currentIndex * 100;
-  carousel.style.transform = `translateX(${offset}%)`;
-}
-
-prevBtn.addEventListener("click", () => {
-  currentIndex = (currentIndex - 1 + items.length) % items.length;
-  updateCarousel();
-});
-
-nextBtn.addEventListener("click", () => {
-  currentIndex = (currentIndex + 1) % items.length;
-  updateCarousel();
-});
-
-// Menú hamburguesa
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector("#nav-menu");
 
